8e625662abf4b573444908c3ba46132676f64107
Jeff Guillou <jean.f.guillou@gmail.com>
2023-05-22 16:12:55 +0200

    Started settings_screen with settings_ui

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:gulpi/widgets/app_drawer.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsScreen extends StatelessWidget {
   static const name = "SettingsScreen";
@@ -12,7 +13,35 @@ class SettingsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(l10n.settings)),
       drawer: const AppDrawer(),
-      body: Placeholder(),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.language),
+                title: Text(l10n.language),
+                value: Text('English'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: Text('Server'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.link),
+                title: Text('GLPI server URL'),
+                value: Text('https://glpi.example.com'),
+              ),
+              SettingsTile.navigation(
+                leading: const Icon(Icons.lock_outline),
+                title: Text('Token'),
+                value: Text('(not set)'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
